e0cdab7810a6e71a8fe439aef498db0f97cf9ebb
Cameron Dykes <yellow5c@gmail.com>
2016-10-05 17:24:49 -0500

    Update KVServer.start/2 to launch child task accepting tcp socket

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -6,12 +6,11 @@ defmodule KVServer do
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
+    import Supervisor.Spec
 
     # Define workers and child supervisors to be supervised
     children = [
-      # Starts a worker by calling: KVServer.Worker.start_link(arg1, arg2, arg3)
-      # worker(KVServer.Worker, [arg1, arg2, arg3]),
+      worker(Task, [KVServer, :accept, [4040]]),
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
