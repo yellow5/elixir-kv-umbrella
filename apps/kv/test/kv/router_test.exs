e557f19feaaf338e7382d77fb8250c9cce70fbae
Cameron Dykes <yellow5c@gmail.com>
2016-10-05 21:54:18 -0500

    Tag the distributed test and exclude it from typical test runs

--- apps/kv/test/kv/router_test.exs
+++ apps/kv/test/kv/router_test.exs
@@ -1,6 +1,7 @@
 defmodule KV.RouterTest do
   use ExUnit.Case, async: true
 
+  @tag :distributed
   test "route requests across nodes" do
     assert KV.Router.route("hello", Kernel, :node, []) == :"foo@vagrant-jessie64"
     assert KV.Router.route("world", Kernel, :node, []) == :"bar@vagrant-jessie64"
